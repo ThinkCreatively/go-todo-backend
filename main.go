66732739d7775b74abfd304f12fe365d0eb7ba25
James Moore <jameswmoore99@gmail.com>
2024-01-24 13:19:21 -0700

    Got basic get request set up, cors taken cared of

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
-	"errors"
-	"github.com/gin-gonic/gin"
 	"net/http"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	// "errors"
 )
 
 type rowData struct {
@@ -16,4 +18,19 @@ var rows = []rowData{
 	{ID: "1", Title: "Row One", Value: "this is the value of row one"},
 	{ID: "2", Title: "Row two", Value: "this is the value of row two"},
 	{ID: "3", Title: "Row Three", Value: "this is the value of row three"},
+}
+
+func getRows(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, rows)
+}
+
+func main() {
+	router := gin.Default()
+	router.Use(cors.New(cors.Config{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{"GET", "POST", "PUT", "PATCH", "DELETE"},
+		AllowHeaders: []string{"Content-Type,access-control-allow-origin, access-control-allow-headers"},
+	}))
+	router.GET("/rows", getRows)
+	router.Run("localhost:8080")
 }
